6d5632aae1dca20b468ad9248cbdb22bc125ccfe
Austin <joha0033@gmail.com>
2018-03-01 10:48:40 -0700

    header + tests

---
+++ src/components/shared/Header.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import './style.header.css'
+import logo from '../../assets/booksSVG.svg';
+import profile from '../../assets/profile.svg';
+
+class Header extends React.Component{
+  constructor(){
+    super()
+    this.state = {}
+  }
+
+  render() {
+    return(
+      <nav>
+        <div className="capsule">
+          <img src={logo} className="Nav-logo" alt="logo"/>
+           <ul id="navLinks">
+             <li>
+               <a href="#home">Home</a>
+             </li>
+             <li>
+               <a href="#about">About</a>
+             </li>
+             <li>
+               <a href="#contact">Contact</a>
+             </li>
+           </ul>
+             <img src={profile} className="Nav-profile" alt="logo"/>
+        </div>
+      </nav>
+    )
+  }
+}
+
+export default Header
---
+++ src/components/shared/Header.test.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Header from './Header';
+import Enzyme from 'enzyme';
+import {shallow} from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+
+Enzyme.configure({ adapter: new Adapter() })
+
+it('Header renders', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Header />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders header links', () => {
+  const header = shallow(<Header />)
+  let length = header.find('li').length
+  expect(length).toBe(3)
+})
+
+it('renders a logo', () => {
+  const header = shallow(<Header />)
+  let length = header.find('img').length
+  expect(length).toBe(2)
+})
